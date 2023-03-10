f65433ead476853d18bf6a69303c4ec42855211d
haizler <haizler.mykhailo@gmail.com>
2023-03-10 16:24:31 +0200

    first commit

---
+++ Lesson1/script1.js
@@ -0,0 +1,21 @@
+let city = 'Київ';
+city = 'Львів';
+let adress = city;
+alert(adress);
+
+
+let n = 2;
+let result = n*n*n;
+console.log(result);
+
+
+let pen = 4 ;
+let penprice = 5.25;
+let pencost = pen*penprice;
+
+let pencil = 6;
+let pencilprice = 3.50;
+let pencilcost = pencil*pencilprice;
+
+let totalcost = pencost+pencilcost;
+document.write(totalcost)
